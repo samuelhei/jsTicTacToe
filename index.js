ab7c063824474b33851e02579b0fdd0235292650
samuelhei <ti.samuelh@gmail.com>
2017-02-06 21:19:05 +0000

    Create webview and update SamCalculator player

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+var app = express();
+
+const bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){ 
+    res.render('index');
+});
+
+app.get('/newGame', function(req, res){
+    var board = require('./board');
+    var playerX = require('./players/SamCalculator');
+    var gameBoard = new board(6,6);
+    
+    var game = {
+      "free" : gameBoard.getFreeChar(),
+    };
+    
+    game["board"] = gameBoard.getBoard();
+    game["you"] = 'X';
+    game["opponent"] = 'O';
+    
+    var player = playerX(JSON.parse(JSON.stringify(game)));
+    gameBoard.play(player.play[0],player.play[1]);
+    res.json(gameBoard.getBoard());
+});
+
+app.post('/play', function(req, res){
+    
+    var board = require('./board');
+    var playerX = require('./players/SamCalculator');
+    var gameBoard = new board(6,6);
+    
+    var game = {
+      "free" : gameBoard.getFreeChar(),
+    };
+    
+    gameBoard.setBoard(req.body.board);
+    game["board"] = gameBoard.getBoard();
+    game["you"] = 'X';
+    game["opponent"] = 'O';
+    if(gameBoard.havefreeSpaces()) {
+        var player = playerX(JSON.parse(JSON.stringify(game)));
+        gameBoard.play(player.play[0],player.play[1]);
+    }
+    res.json({board:gameBoard.getBoard(), 
+              countX: gameBoard.count('X'),
+              countO: gameBoard.count('O'),
+              finished: !gameBoard.havefreeSpaces()});
+});
+
+app.listen(process.env.PORT || 3000);
--- players/SamCalculator.js
+++ players/SamCalculator.js
@@ -1,52 +1,207 @@
+function bestPlay(free, board, person, check) {
+    var testBoard = createTestBoard(board);
+    
+    var w = testBoard[0].length;
+    var h = testBoard.length;
+    
+    var scoreBoard = createScoreBoard(w,h);
+    var comb = combinacoes(0,free.length-1, check > free.length-1 ? free.length-1 : check,0,400000);
+    for(var a in comb) {
+
+        for(var b in comb[a]) {
+            testBoard[free[comb[a][b]][0]][free[comb[a][b]][1]] = person;
+        }
+        var points = count(testBoard, person);
+        for(b in comb[a]) {
+            scoreBoard[free[comb[a][b]][0]][free[comb[a][b]][1]] += points;
+        }
+        testBoard = createTestBoard(board);
+    }
+    
+    var high = 0;
+    var cordn = [];
+
+    for (var r = 0; r < h; r++) {
+        for (var c = 0; c < w; c++) {
+            if (scoreBoard[r][c] > high) {
+                high = scoreBoard[r][c];
+                cordn = [r, c];
+            }
+        }
+    }
+    return {cordn:cordn,score:high};
+}
+
+
+function createTestBoard(board) {
+    return JSON.parse(JSON.stringify(board));
+}
+
+function createScoreBoard(w,h) {
+    var scoreBoard = [];
+    
+    for (var r = 0; r < h; r++) {
+        for (var c = 0; c < w; c++) {
+            if (typeof(scoreBoard[r]) == 'undefined') {
+                scoreBoard[r] = [];
+            }
+            scoreBoard[r][c] = 0;
+        }
+    }
+    return scoreBoard;
+}
+
+function play(move) {
+    return {
+            "name": "SamCalculator",
+            "play": move 
+           };
+}
+
+function quantas(n, m) {
+    m = ( m < n - m ? m : n - m );
+    var dividendo = 1;
+    var divisor = 1;
+    for ( var i = 2 ; i <= n ; i++ ) {
+        if ( i <= n-m && i <= m )
+            divisor *= i;
+        if ( m < i && n-m < i )
+            dividendo *= i;
+    }
+    return dividendo / divisor;
+}
+
+function combinacoes(a, b, m, inicio, fim, acc, retorno) {
+    if ( inicio === undefined ) inicio = 0;
+    if ( fim === undefined ) fim = quantas(b-a+1, m);
+    if ( fim <= 0 )
+        return retorno;
+
+    if ( acc == undefined ) acc = [];
+    if ( retorno === undefined ) retorno = [];
+    if ( m == 0 ) {
+        retorno.push(acc);
+        return retorno;
+    }
+    if ( a > b )
+        return retorno;
+
+    // Primeiro fazemos todas as combinações que incluem a
+    if ( inicio < quantas(b-a, m-1) )
+        combinacoes(a+1, b, m-1, inicio, fim, acc.concat([a]), retorno);
+    // Depois todas as que não incluem a
+    inicio -= quantas(b-a, m-1);
+    fim -= quantas(b-a, m-1);
+    return combinacoes(a+1, b, m, inicio, fim, acc, retorno);
+}
+
 var freeSpaces = function(board) {
     var w = board[0].length;
     var h = board.length;
-    
+
     var free = [];
-    
-    for(var r=0;r<h;r++) {
-        for(var c=0;c<w;c++) {
-            if(board[r][c] == ' ') {
-                free.push([r,c]);
+
+    for (var r = 0; r < h; r++) {
+        for (var c = 0; c < w; c++) {
+            if (board[r][c] == ' ') {
+                free.push([r, c]);
             }
         }
     }
     return free;
 }
 
-var count = function(board,type) {
-    var count = 0;
-    
+var freeSpacesFiltered = function(board) {
     var w = board[0].length;
     var h = board.length;
-    
-    for(var r=0;r<h;r++) {
-        for(var c=0;c<w;c++) {
-            if(board[r][c] !== type) {
-                continue;
-            }
-            
-            var noFirstOrLastColumn = c > 0 && c < (w-1);
-            var noFirstOrLastRow = r > 0 && r < (h-1);
-            
-            if(noFirstOrLastColumn) {
-                if(board[r][c-1] === type && board[r][c+1] === type) {
-                    ++count;
-                }
-            }
-            
-            if(noFirstOrLastRow) {
-                if(board[r-1][c] === type && board[r+1][c] === type) {
-                    ++count; 
-                }               
-            }
-            
-            if(noFirstOrLastColumn && noFirstOrLastRow) {
-                if(board[r-1][c-1] === type && board[r+1][c+1] === type) {
-                    ++count;
+
+    var rows = [];
+    var columns = [];
+
+    var free = [];
+    var filtered = [];
+
+    for (var r = 0; r < h; r++) {
+        for (var c = 0; c < w; c++) {
+            if (board[r][c] != ' ') {
+                if(rows.length == 0 || rows[rows.length-1] != r) {
+                    rows.push(r);
                 }
                 
-                if(board[r+1][c-1] === type && board[r-1][c+1] === type) {
+                if(columns.length == 0 || columns[columns.length-1] != c) {
+                    columns.push(c);
+                }
+            }
+            else {
+                free.push([r,c]);
+            }
+        }
+    }
+
+    if(free.length == r*c) {
+        return [[2,2]];
+    }
+
+    for (var r = 0; r < h; r++) {
+        for (var c = 0; c < w; c++) {
+            if (board[r][c] == ' ') {
+                
+                for(var row in rows) {
+                    var add = false;
+                    for(var col in columns) {
+                        if(c > columns[col] -3  && 
+                           c < columns[col] +3 &&
+                           r > rows[row] - 3    && 
+                           r < rows[row] +3) {
+                            filtered.push([r,c]);
+                            add = true;
+                            break;
+                        }    
+                    }
+                    if(add) {
+                        break;
+                    }
+                }
+                
+            }
+        }
+    }
+    return filtered;
+}
+
+var count = function(board, type) {
+    var count = 0;
+
+    var w = board[0].length;
+    var h = board.length;
+
+    for (var r = 0; r < h; r++) {
+        for (var c = 0; c < w; c++) {
+            if (board[r][c] !== type) {
+                continue;
+            }
+
+            var noFirstOrLastColumn = c > 0 && c < (w - 1);
+            var noFirstOrLastRow = r > 0 && r < (h - 1);
+
+            if (noFirstOrLastColumn) {
+                if (board[r][c - 1] === type && board[r][c + 1] === type) {
+                    ++count;
+                }
+            }
+
+            if (noFirstOrLastRow) {
+                if (board[r - 1][c] === type && board[r + 1][c] === type) {
+                    ++count;
+                }
+            }
+
+            if (noFirstOrLastColumn && noFirstOrLastRow) {
+                if (board[r - 1][c - 1] === type && board[r + 1][c + 1] === type) {
+                    ++count;
+                }
+
+                if (board[r + 1][c - 1] === type && board[r - 1][c + 1] === type) {
                     ++count;
                 }
             }
@@ -56,124 +211,28 @@ var count = function(board,type) {
 };
 
 module.exports = function(game) {
-    var testBoard = JSON.parse(JSON.stringify(game.board));
-    var scoreBoard = [];
-
+    var free = freeSpaces(game.board);
+    //console.log(free);
+    //return play([0,0]);
+    
     var w = game.board[0].length;
     var h = game.board.length;
     
-    for(var r=0;r<h;r++) {
-        for(var c=0;c<w;c++) {
-            if(typeof(scoreBoard[r]) == 'undefined'){
-                scoreBoard[r] = [];
-            }
-            scoreBoard[r][c] = 0;
-        }
+    if(w*h == free.length) {
+        return play([2,2]);
     }
     
-    var free = freeSpaces(game.board);
-    
-    var highMe = 0;
-    var highOp = 0;
-    var cordnMe = [0,0];
-    var cordnOp = [0,0];
-    
-    var initMe = count(testBoard, game.you);
-    var initOp = count(testBoard, game.opponent);
-    
-    for(var a in free) {
-        testBoard[free[a][0]][free[a][1]] = game.you;
-        var pointsMe = count(testBoard, game.you);
-        testBoard = JSON.parse(JSON.stringify(game.board));
-        testBoard[free[a][0]][free[a][1]] = game.opponent;
-        var pointsOp = count(testBoard, game.opponent);
-        
-        if(pointsMe > highMe) {
-            highMe = pointsMe;
-            cordnMe = free[a];
-        }
-        
-        if(pointsOp > highOp) {
-            highOp = pointsOp;
-            cordnOp = free[a];
-        }
-        testBoard = JSON.parse(JSON.stringify(game.board));
+    if(free.length == 1) {
+        return play(free[0]);
     }
     
-    var totalMe = highMe - initMe;
-    var totalOp = highOp - initOp;
+    var op = bestPlay(free,game.board,game.opponent, 9)
+    var me = bestPlay(free,game.board,game.you, 9)
     
-    var goFree = false;
-
-    if(totalOp > totalMe && totalOp > 1) {
-        cordn = cordnOp;
-        goFree = true;
-    }
-    else if(totalMe > 1) {
-        cordn = cordnMe;
-        goFree = true;
-    }
-    
-    if(goFree) {
-        return {
-            "name" : "SamCalculator",
-            "play" : cordn //[row,column] 
-        };
-    }
-    
-    if(free.length >= 3){
-        for(var a in free) {
-            for(var b in free) {
-                if(b<a) continue;
-                for(var c in free) {
-                    if(c<b) continue;
-                    
-                    testBoard[free[a][0]][free[a][1]] = game.you;
-                    testBoard[free[b][0]][free[b][1]] = game.you;
-                    testBoard[free[c][0]][free[c][1]] = game.you;
-                    
-                    var points = count(testBoard, game.you);
-                    
-                    scoreBoard[free[a][0]][free[a][1]] += points;
-                    scoreBoard[free[b][0]][free[b][1]] += points;
-                    scoreBoard[free[c][0]][free[c][1]] += points;
-                    
-                    testBoard = JSON.parse(JSON.stringify(game.board));
-                }
-            }
-        }
-        
-        var high = 0;
-        var cordn = [];
-        
-        for(var r=0;r<h;r++) {
-            for(var c=0;c<w;c++) {
-                if(scoreBoard[r][c] > high) {
-                    high = scoreBoard[r][c];
-                    cordn = [r,c];
-                }
-            }
-        }
+    if(op.score > me.score) {
+        return play(op.cordn);
     }
     else {
-        var high = 0;
-        var cordn = [0,0];
-        
-        for(var a in free) {
-        
-            testBoard[free[a][0]][free[a][1]] = game.you;
-            var points = count(testBoard, game.you);
-            
-            if(points > high) {
-                high = points;
-                cordn = free[a];
-            }
-            testBoard = JSON.parse(JSON.stringify(game.board));
-        }
+        return play(me.cordn);
     }
-    
-    return {
-        "name" : "SamCalculator",
-        "play" : cordn //[row,column] 
-    };
 }
